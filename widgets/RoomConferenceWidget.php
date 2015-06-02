a53c7a3255ba116faf1167a32850bddd92c21f3e
GQAdonis <gqadonis@me.com>
2015-06-02 02:09:29 -0500

    janus added

--- widgets/RoomConferenceWidget.php
+++ widgets/RoomConferenceWidget.php
@@ -20,42 +20,44 @@ class RoomConferenceWidget extends HWidget {
         Yii::app()->clientScript->setJavascriptVariable('confRoomName', $this->room->name);
         //Yii::app()->clientScript->registerScriptFile($assetPrefix . '/require.js');
         Yii::app()->clientScript->registerScriptFile($assetjqueryuiPrefix . '/jquery-ui.min.js');
-        Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.min.js');
-        Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.disco.min.js');
-        Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.caps.jsonly.min.js');
-        Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/rayo.js');
-        Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/tooltip.js');
-        Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/popover.js');
-        Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/toastr.js');
-        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/config.js');
-        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/interface_config.js');
-        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/app.bundle.js');
-        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/analytics.js');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/font.css?v=5');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/toastr.css?v=1');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/main.css?v=29', 'screen');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/videolayout_default.css?v=13', 'screen', array('id' => 'videolayout_default'));
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/jquery-impromptu.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/modaldialog.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/notice.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/popup-menu.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/login_menu.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/popover.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/jitsi_popover.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/contact_list.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/chat.css');
+        //Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.min.js');
+        //Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.disco.min.js');
+        //Yii::app()->clientScript->registerScriptFile($assetStrophePrefix . '/strophe.caps.jsonly.min.js');
+        Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/bootbox.min.js');
+        //Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/tooltip.js');
+        //Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/popover.js');
+        //Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/toastr.js');
+        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/adapter.js');
+        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/janus.js');
+        Yii::app()->clientScript->registerScriptFile($assetPrefix . '/mcu.js');
+        //Yii::app()->clientScript->registerScriptFile($assetPrefix . '/interface_config.js');
+        //Yii::app()->clientScript->registerScriptFile($assetPrefix . '/app.bundle.js');
+        //Yii::app()->clientScript->registerScriptFile($assetPrefix . '/analytics.js');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/font.css?v=5');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/toastr.css?v=1');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/main.css?v=29', 'screen');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/videolayout_default.css?v=13', 'screen', array('id' => 'videolayout_default'));
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/jquery-impromptu.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/modaldialog.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/notice.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/popup-menu.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/login_menu.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/popover.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/jitsi_popover.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/contact_list.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/chat.css');
         Yii::app()->clientScript->registerCssFile($assetjqueryuiPrefix . '/jquery-ui.min.css');
         Yii::app()->clientScript->registerCssFile($assetjqueryuiPrefix . '/jquery-ui.structure.min.css');
         Yii::app()->clientScript->registerCssFile($assetjqueryuiPrefix . '/jquery-ui.theme.min.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/welcome_page.css');
-        Yii::app()->clientScript->registerCssFile($assetPrefix . '/settings_menu.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/welcome_page.css');
+        //Yii::app()->clientScript->registerCssFile($assetPrefix . '/settings_menu.css');
         Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/jquery-impromptu.js');
         Yii::app()->clientScript->registerScriptFile($assetLibsPrefix . '/jquery.autosize.js');
     }
 
     public function run()
     {
-        $this->render('roomConference', array('room' => $this->room));
+        $this->render('janusConference', array('room' => $this->room));
     }
 }
 
