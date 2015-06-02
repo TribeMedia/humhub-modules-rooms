7f34229919f1e54ae190379d0d842e4f4846dd81
GQAdonis <gqadonis@me.com>
2015-06-02 05:43:48 -0500

    fixed the address on janus server

--- resources/conference/mcu.js
+++ resources/conference/mcu.js
@@ -50,7 +50,7 @@ var server = null;
 if(window.location.protocol === 'http:')
     server = "http://" + window.location.hostname + ":8088/janus";
 else
-    server = "https://" + window.location.hostname + ":8089/janus";
+    server = "https://" + window.location.hostname + ":8889/janus";
 
 var janus = null;
 var mcutest = null;
